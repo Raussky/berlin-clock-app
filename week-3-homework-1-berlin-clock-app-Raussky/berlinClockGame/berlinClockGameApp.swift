bb55414c149869c85a89f59a3f92c4033097c717
Rauan Gadil <104556704+Raussky@users.noreply.github.com>
2024-05-13 16:32:37 +0500

    Add files via upload

---
+++ week-3-homework-1-berlin-clock-app-Raussky/berlinClockGame/berlinClockGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  berlinClockGameApp.swift
+//  berlinClockGame
+//
+//  Created by Admin on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct berlinClockGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
